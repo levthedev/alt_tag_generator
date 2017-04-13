b4aa0f5343fab2e7bef19a5118d59ab96bffcec1
Lev Kravinsky <kravinskylev@gmail.com>
2017-04-13 19:03:30 -0400

    process images and add alt tags using google vision api

--- background.js
+++ background.js
@@ -1,6 +1,33 @@
-console.log('hi')
+key = ''
+var url = "https://vision.googleapis.com/v1/images:annotate?key=" + key
 var images = document.querySelectorAll('img');
+var requests = []
+
 images.forEach(function(img) {
-  console.log(img, img.alt)
-  img.alt = 'yo'
+  var imgRequest = { "image": { "source": { "imageUri": img.src } }, "features": [ { "type": "LABEL_DETECTION" } ] }
+  requests.push(imgRequest)
+})
+
+var finalRequest = { "requests": requests }
+function success(response) {
+  response.responses.forEach(function(response, i) {
+    if (response.labelAnnotations) {
+      var alt = response.labelAnnotations.map(function(label) {
+        console.log(label.description)
+        return label.description
+      })
+      images[i].alt = alt.join(', ')
+    } else if (response.error) {
+      images[i].alt = 'Error: no alt tag could be generated'
+    }
+  })
+}
+
+$.ajax({
+  type: "POST",
+  url: url,
+  data: JSON.stringify(finalRequest),
+  success: success,
+  dataType: "json",
+  headers: { "Content-Type": "application/json" }
 })
